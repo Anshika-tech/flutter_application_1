b90dd40e3fb44fa91b869da93c5072b71d4d74c4
Anshika-tech <srivastavanshika10@gmail.com>
2022-06-23 14:18:48 +0530

    practice

--- lib/ButtonDemo.dart
+++ lib/ButtonDemo.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 class ButtonDemo extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
@@ -8,6 +8,57 @@ class ButtonDemo extends StatelessWidget{
         child:Container(
           child:Text("this is body",style:TextStyle(fontSize:30),
           ),
+        ),
+      ),
+      
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        print("Floating Action button clicked");
+      },
+      child:Icon(Icons.add),
+      ),
+      )
+      
+  ,);
+  }
+}*/
+//all types of button
+import 'package:flutter/material.dart';
+class ButtonDemo extends StatelessWidget{
+  const ButtonDemo({Key? key}) : super(key: key);
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:Scaffold(
+      body:SafeArea(
+        child:Center(
+        child:Container(
+          child:Column(
+            children:[
+               
+                ElevatedButton(onPressed:(){
+                  print("elevated button clicked");
+                },child:
+                Text("Login",style:TextStyle(fontSize:24),),
+                ),
+
+                 OutlinedButton(onPressed:(){
+                  print("Outlined button clicked");
+                 },child:
+                Text("Signup",style:TextStyle(fontSize:24),),
+                ),
+
+                 IconButton(onPressed:(){
+                  print("Icon button clicked");
+                 },
+                 icon:Icon(Icons.alarm,size:50,color:Colors.blue,),),
+                 
+                TextButton(onPressed:(){
+                  print("Text button clicked");
+                },child:
+                Text("register",style:TextStyle(fontSize:24),),
+                ),
+            ],
+          ),
+        ),
         ),
       ),
       
